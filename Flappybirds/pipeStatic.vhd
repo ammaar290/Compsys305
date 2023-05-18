a9261ac2f0d600115400bb41264e79050a88d052
Khaled Shaik <74567668+shazzykazzy@users.noreply.github.com>
2023-05-18 13:34:40 +1200

    Update pipeStatic.vhd

--- Flappybirds/pipeStatic.vhd
+++ Flappybirds/pipeStatic.vhd
@@ -13,22 +13,28 @@ END pipeStatic;
 architecture behavior of pipeStatic is
 
 SIGNAL ball_on					: std_logic;
-SIGNAL size 					: std_logic_vector(9 DOWNTO 0);  
+SIGNAL sizex 					: std_logic_vector(9 DOWNTO 0);  
+SIGNAL sizey 					: std_logic_vector(9 DOWNTO 0); 
 SIGNAL ball_y_pos, ball_x_pos	: std_logic_vector(9 DOWNTO 0);
 
 BEGIN           
 
-size <= CONV_STD_LOGIC_VECTOR(200,20);  -- changin to a rectanglular pipe spanning vertically
-ball_x_pos <= CONV_STD_LOGIC_VECTOR(840,10);  -- move pipe to the corner
+sizex <= CONV_STD_LOGIC_VECTOR(20,10);  -- changin to a rectanglular pipe spanning vertically
+sizey <= CONV_STD_LOGIC_VECTOR(200,10);
+
+
+ball_x_pos <= CONV_STD_LOGIC_VECTOR(590,10);
 ball_y_pos <= CONV_STD_LOGIC_VECTOR(350,10);
 
-ball_on <= '1' when ( ('0' & ball_x_pos <= pixel_column + size) and ('0' & pixel_column <= ball_x_pos)  -- x_pos <= pixel_column <= x_pos + size
-					and ('0' & ball_y_pos <= pixel_row + size) and ('0' & pixel_row <= ball_y_pos + size) )  -- y_pos - size <= pixel_row <= y_pos + size
-			else '0';
+ball_on <= '1' when ( ('0' & ball_x_pos <= pixel_column + sizex) and ('0' & pixel_column <= ball_x_pos + sizex) 	-- x_pos - size <= pixel_column <= x_pos + size
+					and ('0' & ball_y_pos <= pixel_row + sizey) and ('0' & pixel_row <= ball_y_pos + sizey) )  else	-- y_pos - size <= pixel_row <= y_pos + size
+			'0';
+
 
 -- Colours for pixel data on video signal
--- Keeping background white and square in green
-Red <=  not ball_on;  -- Change the color to green
+-- Keeping background white and square in red
+Red <=  not ball_on;
+-- Turn off Green and Blue when displaying square
 Green <= '1';
 Blue <=  not ball_on;
 

c3f9b10b20d86695b2d9e337a802163d009630d3
cshu401 <100852844+cshu401@users.noreply.github.com>
2023-05-22 14:27:32 +1200

    bouncyBird.vhd redbackground ball

--- Flappybirds/bouncyBird.vhd
+++ Flappybirds/bouncyBird.vhd
@@ -32,8 +32,8 @@ ball_on <= '1' when ( ('0' & ball_x_pos <= '0' & pixel_column + size) and ('0' &
 
 -- Colours for pixel data on video signal
 -- Changing the background and ball colour by pushbuttons
-Red <=  pb1;
-Green <= (not pb2) and (not ball_on);
+Red <=  1;
+Green <= not ball_on;
 Blue <=  not ball_on;
 
 

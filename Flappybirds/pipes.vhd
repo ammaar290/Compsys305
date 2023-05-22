2891140c6536f274ae49cb12baf0618c46081d1f
Ammaar <amar290@aucklanduni.ac.nz>
2023-05-22 21:15:34 +1200

    update pipes.vhd

--- Flappybirds/pipes.vhd
+++ Flappybirds/pipes.vhd
@@ -4,11 +4,12 @@ USE IEEE.STD_LOGIC_ARITH.all;
 USE IEEE.STD_LOGIC_UNSIGNED.all;
 
 ENTITY pipes IS
-	PORT
-		( clk 						: IN std_logic;
-		  pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
-		  red, green, blue 			: OUT std_logic);		
+    PORT
+        ( clk                      : IN std_logic;
+          pixel_row, pixel_column  : IN std_logic_vector(9 DOWNTO 0);
+          red, green, blue         : OUT std_logic);     
 END pipes;
+
 architecture behavior of pipes is
 
 SIGNAL ball_on  : std_logic;
@@ -21,13 +22,15 @@ BEGIN
 
 sizex <= CONV_STD_LOGIC_VECTOR(20,10);  -- changing to a rectangular pipe spanning vertically
 sizey <= CONV_STD_LOGIC_VECTOR(200,10);
-
-ball_x_pos <= CONV_STD_LOGIC_VECTOR(590,10);
 ball_y_pos <= CONV_STD_LOGIC_VECTOR(350,10);
 
 process (clk)  -- New process to update ball_x_pos
 begin
     if rising_edge(clk) then
+        if counter = 0 then
+            ball_x_pos <= CONV_STD_LOGIC_VECTOR(590,10);  -- Initialize the pipe's x-coordinate to its starting position
+        end if;
+
         counter <= counter + 1;  -- Increment the counter
 
         if counter >= 10000 then  -- If the counter reaches 10000...
@@ -42,8 +45,8 @@ begin
     end if;
 end process;
 
-ball_on <= '1' when ( ('0' & ball_x_pos <= pixel_column + sizex) and ('0' & pixel_column <= ball_x_pos + sizex) 	-- x_pos - size <= pixel_column <= x_pos + size
-                    and ('0' & ball_y_pos <= pixel_row + sizey) and ('0' & pixel_row <= ball_y_pos + sizey) )  else	-- y_pos - size <= pixel_row <= y_pos + size
+ball_on <= '1' when ( ('0' & ball_x_pos <= pixel_column + sizex) and ('0' & pixel_column <= ball_x_pos + sizex)  -- x_pos - size <= pixel_column <= x_pos + size
+                    and ('0' & ball_y_pos <= pixel_row + sizey) and ('0' & pixel_row <= ball_y_pos + sizey) )  else    -- y_pos - size <= pixel_row <= y_pos + size
             '0';
 
 

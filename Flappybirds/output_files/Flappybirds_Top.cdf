ea668097917047a0b732203205abfd514b2c7447
Khaled Shaik <74567668+shazzykazzy@users.noreply.github.com>
2023-05-23 15:15:56 +1200

    PipesDynamicWorking
    It works

--- Flappybirds/output_files/Flappybirds_Top.cdf
+++ Flappybirds/output_files/Flappybirds_Top.cdf
@@ -1,10 +1,10 @@
-/* Quartus Prime Version 18.1.0 Build 625 09/12/2018 SJ Standard Edition */
+/* Quartus Prime Version 18.1.0 Build 625 09/12/2018 SJ Lite Edition */
 JedecChain;
 	FileRevision(JESD32A);
 	DefaultMfr(6E);
 
 	P ActionCode(Cfg)
-		Device PartName(5CEBA4F23) Path("C:/Users/ksha810/Downloads/Compsys305-main/Compsys305-main/Flappybirds/output_files/") File("Flappybirds_Top.sof") MfrSpec(OpMask(1));
+		Device PartName(5CEBA4F23) Path("C:/Users/Khaled/OneDrive/Documents/GitHub/Compsys305/Flappybirds/output_files/") File("Flappybirds_Top.sof") MfrSpec(OpMask(1));
 
 ChainEnd;
 
--- Flappybirds/pipes.vhd
+++ Flappybirds/pipes.vhd
@@ -1,7 +1,6 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.all;
-USE IEEE.STD_LOGIC_ARITH.all;
-USE IEEE.STD_LOGIC_UNSIGNED.all;
+USE IEEE.NUMERIC_STD.all;
 
 ENTITY pipes IS
     PORT
@@ -15,40 +14,34 @@ architecture behavior of pipes is
 SIGNAL ball_on  : std_logic;
 SIGNAL sizex    : std_logic_vector(9 DOWNTO 0);  
 SIGNAL sizey    : std_logic_vector(9 DOWNTO 0); 
-SIGNAL ball_y_pos, ball_x_pos : std_logic_vector(9 DOWNTO 0);
-SIGNAL counter : integer range 0 to 10000 := 0; -- Add a counter
+SIGNAL ball_y_pos, ball_x_pos : std_logic_vector(9 DOWNTO 0) := std_logic_vector(to_unsigned(590,10)); -- Initialize at right edge of screen
+SIGNAL counter : integer range 0 to 333333 := 0;
 
 BEGIN           
 
-sizex <= CONV_STD_LOGIC_VECTOR(20,10);  -- changing to a rectangular pipe spanning vertically
-sizey <= CONV_STD_LOGIC_VECTOR(200,10);
-ball_y_pos <= CONV_STD_LOGIC_VECTOR(350,10);
+sizex <= std_logic_vector(to_unsigned(20,10));  
+sizey <= std_logic_vector(to_unsigned(200,10));
+ball_y_pos <= std_logic_vector(to_unsigned(350,10));
 
 process (clk)  -- New process to update ball_x_pos
 begin
     if rising_edge(clk) then
-        if counter = 0 then
-            ball_x_pos <= CONV_STD_LOGIC_VECTOR(590,10);  -- Initialize the pipe's x-coordinate to its starting position
-        end if;
-
         counter <= counter + 1;  -- Increment the counter
 
-        if counter >= 10000 then  -- If the counter reaches 10000...
-            counter <= 0;  -- Reset the counter
-
-            if ball_x_pos = "0000000000" then  -- If the pipe has moved to the left edge of the screen...
-                ball_x_pos <= CONV_STD_LOGIC_VECTOR(590,10);  -- Reset the pipe's x-coordinate to its starting position
+        if counter >= 333333 then  -- If the counter reaches 333333...
+            if unsigned(ball_x_pos) <= to_unsigned(0,10) then  -- If the pipe has moved to the left edge of the screen...
+                ball_x_pos <= std_logic_vector(to_unsigned(590,10));  -- Reset the pipe's x-coordinate to its starting position
             else
-                ball_x_pos <= std_logic_vector(to_unsigned(to_integer(unsigned(ball_x_pos)) - 1, ball_x_pos'length));  -- Move the pipe one pixel to the left
+                ball_x_pos <= std_logic_vector(unsigned(ball_x_pos) - 1);  -- Move the pipe one pixel to the left
             end if;
+            counter <= 0;  -- Reset the counter after moving the pipe
         end if;
     end if;
 end process;
 
-ball_on <= '1' when ( ('0' & ball_x_pos <= pixel_column + sizex) and ('0' & pixel_column <= ball_x_pos + sizex)  -- x_pos - size <= pixel_column <= x_pos + size
-                    and ('0' & ball_y_pos <= pixel_row + sizey) and ('0' & pixel_row <= ball_y_pos + sizey) )  else    -- y_pos - size <= pixel_row <= y_pos + size
+ball_on <= '1' when ( (unsigned('0' & ball_x_pos) <= unsigned(pixel_column) + unsigned(sizex)) and (unsigned('0' & pixel_column) <= unsigned(ball_x_pos) + unsigned(sizex))  -- x_pos - size <= pixel_column <= x_pos + size
+                    and (unsigned('0' & ball_y_pos) <= unsigned(pixel_row) + unsigned(sizey)) and (unsigned('0' & pixel_row) <= unsigned(ball_y_pos) + unsigned(sizey)) )  else    -- y_pos - size <= pixel_row <= y_pos + size
             '0';
-
 
 -- Colours for pixel data on video signal
 -- Keeping background white and square in red

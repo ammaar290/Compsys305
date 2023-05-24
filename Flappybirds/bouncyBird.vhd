67495430d9407cb9d16229406e5060e4a732b3dd
ammaar290 <amar290@aucklanduni.ac.nz>
2023-05-25 11:36:19 +1200

    update bouncy bird

--- Flappybirds/bouncyBird.vhd
+++ Flappybirds/bouncyBird.vhd
@@ -8,7 +8,7 @@ ENTITY bouncyBird IS
 	PORT
 		( pb1, pb2, clk, vert_sync	: IN std_logic;
           pixel_row, pixel_column	: IN std_logic_vector(9 DOWNTO 0);
-		  red, green, blue 			: OUT std_logic);		
+		  ball : OUT std_logic);
 END bouncyBird;
 
 architecture behavior of bouncyBird is
@@ -16,14 +16,18 @@ architecture behavior of bouncyBird is
 SIGNAL ball_on					: std_logic;
 SIGNAL size 					: std_logic_vector(9 DOWNTO 0);  
 SIGNAL ball_y_pos				: std_logic_vector(9 DOWNTO 0);
+SIGNAL ball_y_pos_T				: std_logic_vector(9 DOWNTO 0);
 SiGNAL ball_x_pos				: std_logic_vector(10 DOWNTO 0);
-SIGNAL ball_y_motion			: std_logic_vector(9 DOWNTO 0);
+SIGNAL ball_y_motion			: std_logic_vector(9 DOWNTO 0):= CONV_STD_LOGIC_VECTOR(10, 10) ;
 
 BEGIN           
 
 size <= CONV_STD_LOGIC_VECTOR(8,10);
 -- ball_x_pos and ball_y_pos show the (x,y) for the centre of ball
 ball_x_pos <= CONV_STD_LOGIC_VECTOR(100,11);
+
+
+
 
 ball_on <= '1' when ( ('0' & ball_x_pos <= '0' & pixel_column + size) and ('0' & pixel_column <= '0' & ball_x_pos + size) 	-- x_pos - size <= pixel_column <= x_pos + size
 					and ('0' & ball_y_pos <= pixel_row + size) and ('0' & pixel_row <= ball_y_pos + size) )  else	-- y_pos - size <= pixel_row <= y_pos + size
@@ -32,9 +36,9 @@ ball_on <= '1' when ( ('0' & ball_x_pos <= '0' & pixel_column + size) and ('0' &
 
 -- Colours for pixel data on video signal
 -- Changing the background and ball colour by pushbuttons
-Red <=  '1';
-Green <= ball_on;
-Blue <=  ball_on;
+--Red <=  '0';
+--Green <= ball_on;
+--Blue <=  '0';
 
 
 Move_Ball: process (vert_sync)  	
@@ -52,18 +56,26 @@ begin
 			ball_y_motion <= CONV_STD_LOGIC_VECTOR(15,10);
 			
 		else
-			ball_y_motion <= ball_y_motion - CONV_STD_LOGIC_VECTOR(1,10);
+			ball_y_motion <= -CONV_STD_LOGIC_VECTOR(2,10);
 		end if;
 		
-		if(ball_y_pos > "0111010110") then
-			ball_y_motion <= CONV_STD_LOGIC_VECTOR(0,10);
-		elsif(ball_y_pos < "0000000100") then
-			ball_y_motion <= CONV_STD_LOGIC_VECTOR(0,10);
-		end if;
+		-- checks for height floor bypass
+--		if (ball_y_pos <= CONV_STD_LOGIC_VECTOR(460,10) - size) then
+--			ball_y_motion <= CONV_STD_LOGIC_VECTOR(3,10);
+--		end if;
+		
 		
 		ball_y_pos <= ball_y_pos + ball_y_motion;
 		
+		
+		
+		
+		
 	end if;
+	
+
+	ball <= ball_on;
+	
 end process Move_Ball;
 
 
